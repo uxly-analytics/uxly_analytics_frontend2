81e07fafe6f727d71ab8ab7238e4e4bd1407c518
fnguyenn <fnguyen3@ucsc.edu>
2024-02-01 18:09:57 -0800

    Added some Organizational Folders and changed Favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-      Barebones
-    </div>
+    <Router>
+        <Routes>
+            {/* Redirect from "/" to "/login" */}
+            <Route path="/" element={<Navigate to="/login" />}/>
+            {/*<Route path="/login" element={<Account/>} />*/}
+            {/* Add more routes as needed */}
+        </Routes>
+      </Router>
   );
 }
 
