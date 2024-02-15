c0d2943dda98c485509146abedcc21fb832884c4
fnguyenn <fnguyen3@ucsc.edu>
2024-02-15 12:18:34 -0800

    Added Wallet function

--- src/Components/HomePage/Home.tsx
+++ src/Components/HomePage/Home.tsx
@@ -14,7 +14,7 @@ function Home() {
     console.log("Chain: ", chain);
     setSearchInput({ address, chain});
     try{
-      setData(await Service.getSingleAddressData(address, chain));
+      setData(await Service.getWalletData(address, chain));
     }catch(error){
       console.error("Error Fetching Data: ", error);
     }
--- src/Services/WalletServices.tsx
+++ src/Services/WalletServices.tsx
@@ -1,92 +1,38 @@
 import axios from "axios";
 
-//const HOST = "http://54.219.64.148:5000/";
-const HOST = "http://localhost:8888";
+const HOST = "http://54.219.64.148:5000/";
+//const HOST = "http://localhost:8888";
 
-interface SingleAddressData{
-    activeChains: any;
-    simplifiedActiveChains: any;
+interface WalletData{
+    address:string;
+    activeChainsSimplified: any;
     nativeBalance: any;
-    ercBalance: any;
     nft: any;
+    tokenBalance: any;
+    transactions: any;
 }
 
-export async function getSingleAddressData(address: string, chain:string): Promise<SingleAddressData> {
+export async function getWalletData(address: string, chain:string): Promise<WalletData>{
     try{
-        const activeChains = await sActiveChains(address);
-        const simplifiedActiveChains = await sSimplifiedActiveChains(address);
-        const nativeBalance = await sNativeBalance(address, chain);
-        const ercBalance = await sErcBalance(address, chain);
-        const nft = await sNFT(address, chain);
-
-        return{
-            activeChains,
-            simplifiedActiveChains,
-            nativeBalance,
-            ercBalance,
-            nft
-        }
-    }catch(error){
-        console.error("Error:", error);
+        const response = await axios.get(`${HOST}/wallet?address=${address}&chain=${chain}`);
+        console.log(response);
         return {
-            activeChains: {},
-            simplifiedActiveChains: {},
-            nativeBalance: {},
-            ercBalance: {},
-            nft: {}
-        };
-    }
-}
-
-//the "s" at the beginning denotes a single address
-//"m" denotes multiple addresses
-//open to change
-export async function sActiveChains(address: string){
-    try{
-        const response = await axios.get(`${HOST}/active-chains/${address}`);
-        return response.data.response.active_chains;
+            address: response.data.walletStats.address,
+            activeChainsSimplified: response.data.walletStats.activeChainsSimplified,
+            nativeBalance: response.data.walletStats.nativeBalance,
+            nft: response.data.walletStats.nft,
+            tokenBalance: response.data.walletStats.tokenBalance,
+            transactions: response.data.walletStats.transactions,
+        }
     }catch (error){
-        console.error('Error:', error);
-        return 0;
-    }
-}
-
-export async function sSimplifiedActiveChains(address: string){
-    try{
-        const response = await axios.get(`${HOST}/active-chains-simplified/${address}`);
-        return response.data.chains;
-    }catch(error){
-        console.error('Error:', error);
-        return 0;
-    }
-}
-
-export async function sNativeBalance(address: string, chain:string){
-    try{
-        const response = await axios.get(`${HOST}/native-balance/${address}/${chain}`);
-        return response.data.balance;
-    }catch(error){
-        console.error('Error:', error);
-        return 0;
-    }
-}
-
-export async function sErcBalance(address: string, chain:string){
-    try{
-        const response = await axios.get(`${HOST}/erc-balance/${address}/${chain}`);
-        return response.data.tokens;
-    }catch(error){
-        console.error('Error:', error);
-        return 0;
-    }
-}
-
-export async function sNFT(address: string, chain:string){
-    try{
-        const response = await axios.get(`${HOST}/nft/${address}/${chain}`);
-        return response.data.nfts;
-    }catch(error){
-        console.error('Error:', error);
-        return 0;
+        console.log('Error: ', error);
+        return {
+            address: address,
+            activeChainsSimplified: {},
+            nativeBalance: {},
+            nft: {},
+            tokenBalance: {},
+            transactions: {},
+        };
     }
 }
