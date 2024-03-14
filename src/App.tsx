7ea63500e006f630c5600ed33429c07a935547a9
aaranguha <aaranguhaca@gmail.com>
2024-03-13 21:30:27 -0700

    Made Landing Page the Main Screen when App is loaded. Need to connect with Sid's login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Components/HomePage/Home";
+import LandingPage from "./Components/LandingPage/LandingPage";
 import Streams from "./Components/StreamsPage/Streams";
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
     <Router>
       <Routes>
         {/* Redirect from "/" to "/login" */}
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         {/*<Route path="/login" element={<LandingPage />} */}
+        <Route path="/" element={<LandingPage />} />
         <Route path="/streams" element={<Streams />} />
         {/* Add more routes as needed */}
       </Routes>
--- src/Components/LandingPage/LandingPage.tsx
+++ src/Components/LandingPage/LandingPage.tsx
@@ -3,21 +3,22 @@ import "./Components/landingPage.css";
 import LoginButton from './LoginButton';
 import Header from './Header';
 
-
-function LandingPage(){
-
-    return(
+function LandingPage() {
+    return (
         <div className="app-container">
             <section className="header-section">
-              <Header />
+                <Header />
+                <h1>Discover In-Depth Insights</h1>
             </section>
             <section className="login-section">
-            <div className="center-content">
-                <LoginButton />
-            </div>
+                <div className="center-content">
+                    <p style={{ marginBottom: '3rem' }}>Gain valuable insights into how your users interact with your applications and compare their behavior across competitor platforms. With UXly's intuitive and visual analytics, you can discover, consolidate, and communicate user needs effortlessly.</p>
+                    <LoginButton />
+                </div>
             </section>
         </div>
-    )
+    );
 }
+
 
 export default LandingPage;
